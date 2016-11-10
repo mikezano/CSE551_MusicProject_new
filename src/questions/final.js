ef1619ed9a646492ea4b52cd89645c5ecc0b4867
Michael Manzano <mikezano@gmail.com>
2016-11-09 23:43:45 -0700

    more

--- src/questions/final.js
+++ src/questions/final.js
@@ -7,23 +7,19 @@ export class Final{
     constructor(router, spotify){
         this.router = router;
         this.spotify = spotify;
-        //this.login();
-        //debugger;
         this.singerResult = [];
+        this.lookFor={
+            genres:[],
+            albumArtists:[]
+        };
     }
 
     login(){
         this.spotify.login().then(data=>{
             console.log("Logged In", data);
-            this.spotifyToken = data;
-            console.log(this.spotifyToken);
-            this.search('artist');
-
-            //this.singerResults =[];
-   
+            this.spotifyToken = data;   
             this.getResults();
         }).catch(e=>{console.log(e)});
-
     }
 
     next()
@@ -32,14 +28,80 @@ export class Final{
     }
 
     getResults(){
+        console.log("Getting Results");
 
-        for(var i in this.userResponses.singers){
-            console.log(this.userResponses.singers[i]);
-            this.search(this.userResponses.singers[i], 'artist', this.singerResult[i]);
-        }
-        
+        this.getArtistResults();
+        this.getAlbumResults();
     }
+
+    getArtistResults(){
+
+        var p1 = this.search(this.userResponses.artist[0], 'artist');
+        var p2 = this.search(this.userResponses.artist[1], 'artist');
+        var p3 = this.search(this.userResponses.artist[2], 'artist');
+
+        Promise.all([p1,p2,p3]).then(values=>{
+            var data = {};
+            data = JSON.parse(values[0]);
+            this.getGenres(data.artists);
+            data = JSON.parse(values[1]);
+            this.getGenres(data.artists);
+            data = JSON.parse(values[2]);
+            this.getGenres(data.artists);                        
+
+            console.log(this.lookFor.genres);
+        });
+    }
+
+    getAlbumResults(){
+
+        var p1 = this.search(this.userResponses.album[0], 'album');
+        var p2 = this.search(this.userResponses.album[1], 'album');
+        var p3 = this.search(this.userResponses.album[2], 'album');
+
+        Promise.all([p1,p2,p3]).then(values=>{
+            var data = {};
+            data = JSON.parse(values[0]);
+            this.getArtistsFromAlbums(data.albums);
+            data = JSON.parse(values[0]);
+            this.getArtistsFromAlbums(data.albums);
+            data = JSON.parse(values[0]);
+            this.getArtistsFromAlbums(data.albums);                                               
+
+            console.log(this.lookFor.albumArtists);
+
+            var result = {};
+            var a = this.lookFor.albumArtists;
+            for(var i = 0; i < a.length; ++i) {
+                if(!result[a[i]])
+                    result[a[i]] = 0;
+                ++result[a[i]];
+            }            
+            console.log(result);
+
+        });
+    }    
     
+    getGenres(artists){
+        for(var item in artists.items){
+            var obj = artists.items[item];
+            for(var i in obj.genres){
+                this.lookFor.genres.push(obj.genres[i]);
+            }
+        }
+    }
+
+    getArtistsFromAlbums(albums){
+
+        for(var item in albums.items){
+            var obj = albums.items[item];
+
+            for(var i in obj.artists){
+                this.lookFor.albumArtists.push(obj.artists[i].name);
+            }
+        }
+    }
+
     attached(){
         this.userResponses = JSON.parse(localStorage.getItem('userResponses'));
         this.login();
@@ -48,12 +110,13 @@ export class Final{
     search(searchTerm, type, result) {
         //https://developer.spotify.com/web-api/search-item/
         //this.searchTerm = this.userResponses.genres[0];
-        
-        this.spotify.search(searchTerm, type).then(data => {
-            result = JSON.parse(data.response);
-            console.log(this.singerResult);
-            debugger;
 
+        return this.spotify.search(searchTerm, type).then(data => {
+           // console.log(data.response);
+            //result = JSON.parse(data.response);
+            //console.log(this.result);
+            this.artistCount++;
+            return data.response;
         });
     }  
 }
--- src/questions/three.js
+++ src/questions/three.js
@@ -5,12 +5,18 @@ import { Router } from 'aurelia-router';
 export class Three{
     constructor(router){
         this.router = router;
+        this.keyVal = ""; 
+        this.keys=[
+            {value:1, name:'C major'},
+            {value:2, name:'A minor'}
+        ]        
     }
 
     next()
     {
+        this.userResponses.key=1;
         localStorage.setItem('userResponses', JSON.stringify(this.userResponses));
-        this.router.navigate("four");
+        this.router.navigate("final");
     }
     
     attached(){
